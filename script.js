f2a0ca4f5013154208a263d4065f45da68e88d16
dune123 <96974209+dune123@users.noreply.github.com>
2024-10-31 13:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
+	// Write your code here
+	   const charCount={};
+
+	   for(let i=0;i<str.length;i++){
+			charCount[str[i]]=(charCount[str[i]]||0)+1;
+	   }
+
+	   for(let x of str){
+		if(charCount[x]==1){
+			return x;
+		}
+	   }
+	   return null;
+   }
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
